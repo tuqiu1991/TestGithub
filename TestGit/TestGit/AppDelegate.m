81a87aa11ad3e9032b1906e4f83aa66b8b7dc329
tuqiu <tuqiu1991@163.com>
2015-07-19 22:14:50 +0800

    test

--- TestGit/TestGit/AppDelegate.m
+++ TestGit/TestGit/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"aaa");
-    NSLog(@"aaa");
+    NSLog(@"bbb");
     return YES;
 }
 
