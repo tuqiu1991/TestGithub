6460be28ee435e3c653a74db98e4c884beb8bc90
Apple <apple@AppledeMacBook-Pro.local>
2015-07-19 22:24:12 +0800

    nslog

--- TestGit/TestGit/FirstViewController.m
+++ TestGit/TestGit/FirstViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"nslog");
+      NSLog(@"nslog");
+      NSLog(@"nslog");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
